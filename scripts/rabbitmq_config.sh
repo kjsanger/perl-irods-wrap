b1d15e38f84474f7b84743226cd99d999534986f
Keith James <kdj@sanger.ac.uk>
2019-11-07 14:16:38 +0000

    WIP

--- scripts/rabbitmq_config.sh
+++ scripts/rabbitmq_config.sh
@@ -5,6 +5,8 @@ sudo rabbitmqctl add_user test_user p455w0rd
 sudo rabbitmqctl set_user_tags test_user administrator
 sudo rabbitmqctl set_permissions -p test_vhost test_user ".*" ".*" ".*"
 
+cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
 ./scripts/rabbitmq_config.pl # sets up queues and exchanges
 
 sudo rabbitmqctl status
